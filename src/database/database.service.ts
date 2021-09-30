bf428458119c4df1db1ee192b9b7c6a25cfa855d
lea.arturi <lea.arturi@gmail.com>
2021-09-29 23:19:44 -0300

    Docker Postgres & DB Module

---
+++ src/database/database.service.ts
@@ -0,0 +1,23 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConnectionOptions } from 'typeorm';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { Configuration } from '../config/config.keys';
+
+export const databaseProviders = [
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    async useFactory(config: ConfigService) {
+      return {
+        ssl: true,
+        type: 'postgres' as const,
+        host: config.get(Configuration.POSTGRES_HOST),
+        username: config.get(Configuration.POSTGRES_USER),
+        password: config.get(Configuration.POSTGRES_PASSWORD),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      } as ConnectionOptions;
+    },
+  }),
+];
